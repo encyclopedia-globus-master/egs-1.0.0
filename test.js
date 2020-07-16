890116b830e20c0a0a2fb1cb6b571ba20d35c46c
encyclopedia-globus-master <68392376+encyclopedia-globus-master@users.noreply.github.com>
2020-07-16 14:47:21 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,29 @@
+/**
+ * ENCYCLOPEDIA GLOBUS SOFTWARE -- GitHub : [encyclopedia-globus-master]
+ *                             project  info
+ * ( version                   : 1.0.0                                 )
+ * ( author                    : GitHub : [encyclopedia-globus-master] )
+ * ( description               : online encyclopedia software          )
+ * ( language                  : JavaScript (main), PHP 7.4.0          )
+ * ( date created              : 16 / 6 / 2020 - ( DD / MM / YYYY )    )
+ * ( location in GitHub        : encyclopedia-globus-master/egs-1.0.0  )
+ *                               file info
+ * ( author                    : GitHub : [encyclopedia-globus-master] )
+ * ( description               : declare $egs variable                 )
+ * ( date uploaded             : 16 / 6 / 2020 - ( DD / MM / YYYY )    )
+ * ( location                  :
+ *     encyclopedia-globus-master
+ *       egs-1.0.0
+ *         (this file)
+ * )
+ * 
+ * The EGS 1.0.0 script is released to you under stipulation that you p-
+ * rovide adequate acknowledgement of your use of these files. It is al-
+ * lowed of you to use these files in whatever manner you wish to. It is
+ * important that you recognize that BY USING THESE FILES, YOU AGREE TH-
+ * AT IF YOUR APPLICATION OF THESE FILES IS ILLEGAL IN ANY JURISDICTION,
+ * YOU AND YOU ONLY ARE RESPONSILE FOR ANY LAW-SUITS OR OTHER LEGAL ACT-
+ * ION CAUSED BECAUSE OF YOUR APPLICATION OF THESE FILES.
+**/
+
+alert( "Scripts are loading properly" );
